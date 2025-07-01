daeeded2bcb8818a967f2c3b1fc9542d78443c3a
rloayzaf <loayza.rolling@gmail.com>
2025-06-30 20:36:21 -0500

    first commit

--- main.py
+++ main.py
@@ -1,2 +1,9 @@
-print(type([]))
+from flask import Flask, render_template
+app = Flask(__name__)
 
+@app.route('/', methods={'GET'})
+def home():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
